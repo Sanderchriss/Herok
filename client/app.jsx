1c763dbee76adb00ce51decc78ff2fb9e21f7224
Sanderchriss <sanderchriss@gmail.com>
2023-10-17 18:28:40 +0200

    Lagt til funksjonalitet

---
+++ client/app.jsx
@@ -0,0 +1,48 @@
+import React, {useState} from "react";
+import Icon from '@mdi/react';
+import { mdiDeleteForever, mdiPlusBox } from '@mdi/js';
+import "./app.css";
+
+
+export function App() {
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState('');
+
+    const handleAddTask = () => {
+        {
+            setTasks([...tasks, newTask]);
+            setNewTask('');
+        }
+    };
+    /*const handleDoneTask = (index) => {
+        setChecked(!isChecked);
+
+
+    }*/
+    const handleDeleteTask = (index) => {
+        const updatedTasks = tasks.filter((_, i) => i !== index);
+        setTasks(updatedTasks);};
+
+    return (<>
+        <header>
+            <h1>To-Do Liste</h1>
+        </header>
+        <main>
+            <input type="text" value={newTask} onChange={(e) => setNewTask(e.target.value)}/>
+            <button onClick={handleAddTask}><Icon path={mdiPlusBox} size={1} /></button>
+            <ul>
+                {tasks.map((task, index) => (
+                    <li key={index}>
+                        <input type="checkbox" />
+                        {task}
+                        <button onClick={() => handleDeleteTask(index)} >
+                            <Icon path={mdiDeleteForever} size={0.5}/>
+                        </button>
+                    </li>))}
+            </ul>
+        </main>
+        <footer>
+            Laget av Kirsti og Sander 🤤
+        </footer>
+    </>)
+}
--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {App} from "./app";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello Reactoo</h1>);
+root.render(<App/>);
